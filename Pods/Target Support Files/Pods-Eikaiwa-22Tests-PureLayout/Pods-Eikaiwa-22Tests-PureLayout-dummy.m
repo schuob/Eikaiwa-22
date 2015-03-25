92e785d7e4b37af818caf48e75b06ca2cfa5e6f1
Steven Chuob <schuob@gmail.com>
2015-03-24 20:51:41 -0700

    adding pod configuration

---
+++ Pods/Target Support Files/Pods-Eikaiwa-22Tests-PureLayout/Pods-Eikaiwa-22Tests-PureLayout-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Eikaiwa_22Tests_PureLayout : NSObject
+@end
+@implementation PodsDummy_Pods_Eikaiwa_22Tests_PureLayout
+@end
---
+++ Pods/Target Support Files/Pods-Eikaiwa-22Tests/Pods-Eikaiwa-22Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Eikaiwa_22Tests : NSObject
+@end
+@implementation PodsDummy_Pods_Eikaiwa_22Tests
+@end
